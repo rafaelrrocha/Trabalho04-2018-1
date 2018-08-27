fff285e47e853f667dbb555aa4a6cc88a943f1f9
Rafael Rocha <rajoca3000@yahoo.com.br>
2018-08-27 01:56:48 -0300

    Finalizando telas e listas.

--- app/src/main/java/trabalho04/ilusoesindustriais/com/trabalho04/CaminhaoActivity.java
+++ app/src/main/java/trabalho04/ilusoesindustriais/com/trabalho04/CaminhaoActivity.java
@@ -2,12 +2,35 @@ package trabalho04.ilusoesindustriais.com.trabalho04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CaminhaoActivity extends AppCompatActivity {
+
+    private ListView listaMarcasCaminhoes;
+    private String[] marcasCaminhoes = {
+            "AGRALE", "BEPOBUS", "CHEVROLET", "CICCOBUS", "DAF",
+            "EFFA-JMC", "FIAT", "FORD", "FOTON", "GMC", "HYUNDAI",
+            "IVECO", "MAN", "MARCOPOLO", "MASCARELLO", "MAXIBUS",
+            "MERCEDES-BENZ", "NAVISTAR", "NEOBUS", "PUMA-ALFA",
+            "SAAB-SCANIA", "SCANIA", "SHACMAN", "SINOTRUCK",
+            "VOLKSWAGEN", "VOLVO", "WALKBUS"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caminhao);
+
+        listaMarcasCaminhoes = findViewById(R.id.listViewCaminhoesId);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                marcasCaminhoes
+        );
+        listaMarcasCaminhoes.setAdapter( adaptador);
+
     }
 }
--- app/src/main/java/trabalho04/ilusoesindustriais/com/trabalho04/MotoActivity.java
+++ app/src/main/java/trabalho04/ilusoesindustriais/com/trabalho04/MotoActivity.java
@@ -2,12 +2,32 @@ package trabalho04.ilusoesindustriais.com.trabalho04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MotoActivity extends AppCompatActivity {
+
+    private ListView listaMarcasMotos;
+    private String[] marcasMotos = {
+            "AGRALE", "BMW", "CALOI", "DAFRA", "DUCATI", "FOX",
+            "GARINNI", "HARLEY-DAVIDSON", "HONDA", "INDIAN",
+            "JIAPENG VOLCANO", "KASINSKI", "KAWASAKI", "LAVRALE",
+            "MOTOCAR", "PEUGEOT", "REGAL RAPTOR", "SUNDOWN",
+            "SUZUKI", "TIGER", "VENTO", "WUYANG", "YAMAHA", "byCristo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moto);
+
+        listaMarcasMotos = findViewById(R.id.listViewMotosId);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                marcasMotos
+        );
+        listaMarcasMotos.setAdapter( adaptador);
     }
 }
